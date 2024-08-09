bd0513a0935543d4f435c35e5a11dbe07c8dce56
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-08-09 13:08:13 +0200

    handlers
    Handlers

---
+++ routehandlerdemo/app/profile/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfilePage = () => {
+  return (
+    <div className="h-screen">
+      <h1 className="text-3xl font-bold m-4">Profile Page</h1>
+    </div>
+  );
+};
+
+export default ProfilePage;
