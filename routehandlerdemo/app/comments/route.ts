ec27fb65091173bed0cb82d66cf3ce7b5e29df4c
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-08-12 09:31:52 +0200

    POST

--- routehandlerdemo/app/comments/route.ts
+++ routehandlerdemo/app/comments/route.ts
@@ -3,3 +3,17 @@ import { comments } from "./data";
 export async function GET() {
   return Response.json(comments);
 }
+
+export async function POST(request: Request) {
+  const comment = await request.json();
+
+  const newComment = {
+    id: comments.length + 1, // comments[] lenght + 1 .. comments array from data
+    text: comment.text, // comment from the request body
+  };
+  comments.push(newComment);
+  return new Response(JSON.stringify(newComment), {
+    headers: { "Content-Type ": "application/json" },
+    status: 201,
+  });
+}
