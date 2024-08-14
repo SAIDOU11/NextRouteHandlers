9b39a593e4043afdd8b899b778cd240240f6c5b8
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-08-14 09:15:41 +0200

    redirect

--- routehandlerdemo/app/comments/route.ts
+++ routehandlerdemo/app/comments/route.ts
@@ -4,10 +4,10 @@ import { comments } from "./data";
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const query = searchParams.get("query");
-  const filteredComments = query
+  const filteredComment = query
     ? comments.filter((comment) => comment.text.includes(query))
     : comments;
-  return Response.json(filteredComments);
+  return Response.json(filteredComment);
 }
 
 export async function POST(request: Request) {
