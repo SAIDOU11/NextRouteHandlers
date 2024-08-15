ed05e3cf38d15188005cb64fd4c625821cde2f57
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-08-15 09:46:48 +0200

    headers & cookies

--- routehandlerdemo/app/profile/api/route.ts
+++ routehandlerdemo/app/profile/api/route.ts
@@ -1,3 +1,23 @@
-export async function GET() {
-  return new Response("Profile API Data");
+import { headers, cookies } from "next/headers";
+import { type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  const headersList = headers();
+
+  cookies().set("ResultsPage", "20");
+  const theme = request.cookies.get("theme");
+
+  console.log(requestHeaders.get("Authorization"));
+  console.log(headersList.get("Authorization"));
+
+  console.log(theme);
+  console.log(cookies().get("ResultsPage"));
+
+  return new Response("<h1>Profile API Data</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
+    },
+  });
 }
